bcd18663b5192f39ad0bf6d31a84fe6abf997fa3
Ablandaeta <ablandaeta@gmail.com>
2025-04-03 09:39:24 +0000

    redo

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our', "my", "your"];
+let adj = ['great', 'juicy', "special", "random"];
+let noun = ['kitty', 'grapes', "rat", 'domain'];
+let extention = [".com", ".net", ".any", '.not.Exe']
 
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let e = 0; e < extention.length; e++) {
+        console.log(pronoun[p] + adj[a] + noun[n] + extention[e])
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function () {
-  //write your code here
-
-  
-
-
-  console.log("Hello Rigo from the console!");
-};
+      }
+    }
+  }
+}
